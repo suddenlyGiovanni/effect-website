325f510f9ed70b1b076289e2148d64529ad756a5
Maxwell Brown <maxwellbrown1990@gmail.com>
2026-03-12 16:41:49 -0400

    move starlight to docs subpath

--- astro.config.ts
+++ astro.config.ts
@@ -22,12 +22,12 @@ export default defineConfig({
           label: "Guides",
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", slug: "guides/example" },
+            { label: "Example Guide", slug: "docs/guides/example" },
           ],
         },
         {
           label: "Reference",
-          autogenerate: { directory: "reference" },
+          autogenerate: { directory: "docs/reference" },
         },
       ],
     }),
