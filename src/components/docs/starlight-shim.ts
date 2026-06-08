1191e21be9812257345a9c20bac5fc0ab95f1cc7
Federico Biccheddu <service+git@f15u.dev>
2026-06-09 00:21:01 +0200

    ui: add `Badge` component to shims

--- src/components/docs/starlight-shim.ts
+++ src/components/docs/starlight-shim.ts
@@ -1,3 +1,4 @@
+export { default as Badge } from "./Badge.astro"
 export { default as Aside } from "./Aside.astro"
 export { default as Steps } from "./Steps.astro"
 export { default as Tabs } from "./Tabs.astro"
